b921927d8c73d795baa2e1327e4ae5b485b6848c
fear-the-lord <souvikghosh199831@gmail.com>
2020-09-29 14:00:53 +0530

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int audioNumber) {
+    final player = AudioCache();
+    player.play('note$audioNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  child: Container(
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  child: Container(
+                    color: Colors.orange,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  child: Container(
+                    color: Colors.yellow,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(4);
+                  },
+                  child: Container(
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(5);
+                  },
+                  child: Container(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  child: Container(
+                    color: Colors.teal,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  child: Container(
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
